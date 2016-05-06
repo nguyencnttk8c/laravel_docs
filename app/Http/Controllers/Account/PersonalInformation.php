b5b90a03c3bd26e96bc311e6cf6cd62effbc0d22
Phi Hung <hungnv0607.dev@gmail.com>
2016-05-07 00:59:29 +0700

    Thêm tài khoản ngân hàng

--- app/Http/Controllers/Account/PersonalInformation.php
+++ app/Http/Controllers/Account/PersonalInformation.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use App\DocBank;
 class PersonalInformation extends Controller{
 
     public function  getIndex () {
@@ -10,7 +11,8 @@ class PersonalInformation extends Controller{
         $user->bank_info = json_decode($user->bank_info);
         $data = array();
         $data['title'] = 'Cập nhật thông tin cá nhân';
-        return view('account.personal-info', compact('data', 'user'));
+        $user_bank =  $user->Bank;
+        return view('account.personal-info', compact('data', 'user', 'user_bank'));
     }
 
     public function postIndex() {
@@ -20,9 +22,26 @@ class PersonalInformation extends Controller{
         } else {
             unset( $user['password']);
         }
-        $user['bank_info'] = json_encode($user['bank_info']);
         $id = \Auth::id();
         $result = User::where('id', $id)->update($user);
+        $new_bank_data = $bank['new'];
+        unset($bank['new']);
+        foreach ($bank as $key=>$b) {
+            if (DocBank::find($key)) {
+                DocBank::where('id', $key)->update($b);
+            } else {
+                DocBank::insert($b);
+            }
+        }
+        foreach ($new_bank_data as $col=>$da) {
+            if ($da == '') {
+                unset($new_bank_data[$col]);
+            }
+        }
+        if (!empty($new_bank_data)) {
+            $new_bank_data['user_id'] = $id;
+            DocBank::insert($new_bank_data);
+        }
         return redirect()->back();
     }
 }
